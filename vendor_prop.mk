d94e90ce72a09d3c8ebbde47b4d858bbd030df67
harry8242 <mohamedharis97@gmail.com>
2018-05-17 15:17:15 +0200

    athene: Fix screen recording
    * This prop is breaking screen recording (gives a solid green video output)
      on all msm8916/52 devices (even in A6020).
    
    This reverts commit b00915058448d8ae5c303b6a60b2a0b0c85bf418.
    
    Change-Id: Ia81e285aa8ce3ccdf6186d65e6130cefb9f4bcb5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -96,7 +96,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.enable_hwc_vds=1 \
 	debug.sf.hw=1 \
 	debug.sf.latch_unsignaled=1 \
 	debug.sf.recomputecrop=0 \
