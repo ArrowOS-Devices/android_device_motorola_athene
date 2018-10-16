e0dc626f738e2ac79f5e393074b7bb78ae84602b
Lyudmila17 <merkel.naller@gmail.com>
2018-10-16 14:23:05 +0200

    athene: hidl: Build our sensors service

--- hidl.mk
+++ hidl.mk
@@ -69,7 +69,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service_moto
 
 # WiFi
 PRODUCT_PACKAGES += \
