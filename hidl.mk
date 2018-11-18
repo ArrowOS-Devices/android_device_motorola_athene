3b227ff7331f69710686335ef40cf3aba6c4a8aa
Lyudmila17 <merkel.naller@gmail.com>
2018-11-18 19:56:00 +0100

    athene: hidl: Build GNSS 1.1
    Change-Id: Iaff16de8dbc23faa86f8bbf08479aedffa834eab

--- hidl.mk
+++ hidl.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti
+    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.1-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
