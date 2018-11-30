2b3c78bd59c1c3d9fd1d18e7d8aa918a2af42844
Rajat Gupta <rjtgupta84@gmail.com>
2018-11-30 22:25:29 +0100

    athene: Update build fingerprint, desc to stock N
    Change-Id: Ia4f2f8820dedd940f2ce38c75cb1b99d84eae46d

--- arrow_athene.mk
+++ arrow_athene.mk
@@ -33,7 +33,7 @@ PRODUCT_MANUFACTURER := Motorola
 BOARD_VENDOR := motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="athene_f-user 7.0 NPJS25.93-14-13 3 release-keys" \
+	PRIVATE_BUILD_DESC="athene_f-user 7.0 NPJ25.93-14-18 3 release-keys" \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "motorola/athene_f/athene_f:7.0/NPJS25.93-14-13/3:user/release-keys" 
+BUILD_FINGERPRINT := "motorola/athene_f/athene_f:7.0/NPJ25.93-14-18/3:user/release-keys"
