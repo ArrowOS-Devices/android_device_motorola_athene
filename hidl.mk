80882996418c0971503f499ca6ea69f530e769c9
Lyudmila17 <merkel.naller@gmail.com>
2018-12-09 18:08:09 +0100

    athene: Build thermal HAL
    Change-Id: If7fe236f24497f0162934b2b930fa8117609cdbb

--- hidl.mk
+++ hidl.mk
@@ -88,6 +88,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service_moto
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
