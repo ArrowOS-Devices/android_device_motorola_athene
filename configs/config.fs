8bed8a44a4144d26805f210a146f667cb0b2e6c5
Brandon Solis <bsolis@codeaurora.org>
2018-12-28 16:06:15 -0800

    athene: Update user for ims daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287

--- configs/config.fs
+++ configs/config.fs
@@ -23,13 +23,13 @@ value:2951
 value:2952
 
 [vendor/bin/imsdatadaemon]
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE
