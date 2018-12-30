73906f1b5c42840c70ce3c4365a9dacb6d76b86b
Lyudmila17 <merkel.naller@gmail.com>
2018-12-30 22:21:45 +0100

    athene: Drop rild shim
    Change-Id: I131543ec3eeaae5b847fb57208c362eb34d2c02d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,6 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /vendor/lib64/lib-imsvt.so|libshims_ims.so \
-    /vendor/lib/libril-qc-qmi-1.so|rild_socket.so \
     /system/lib/hw/camera.msm8952.so|libshims_camera.so \
     /system/lib/libandroid.so|libshim_ril.so \
     /system/lib/libjustshoot.so|libshims_shoot.so
--- device.mk
+++ device.mk
@@ -264,9 +264,6 @@ PRODUCT_PACKAGES += \
     libshim_ril \
     libxml2
 
-PRODUCT_PACKAGES += \
-    rild_socket
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
