398f0f73182234e3e60e5e94ebc943574517843c
Lyudmila17 <merkel.naller@gmail.com>
2019-01-17 00:26:28 +0100

    athene: Reflect P RIL changes
    Change-Id: Iec89620e60fda342d6b4a1b21690bdd55e28d307

--- Android.mk
+++ Android.mk
@@ -172,6 +172,24 @@ $(FIRMWARE_WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_WIDEVINE_SYMLINKS)
 
+PARTITIONS_TO_LINK := dsp fsg
+
+VENDOR_SUBPARTS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/, $(notdir $(PARTITIONS_TO_LINK)))
+$(VENDOR_SUBPARTS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Vendor linked partition: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /$(notdir $@) $@
+
+FIRMWARE_SYMLINK := $(TARGET_OUT_VENDOR)/firmware_mnt
+$(FIRMWARE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Vendor linked partition: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SUBPARTS_SYMLINKS) $(FIRMWARE_SYMLINK)
+
 #########################################################################
 # Create Folder Structure
 #########################################################################
--- hidl.mk
+++ hidl.mk
@@ -80,7 +80,8 @@ PRODUCT_PACKAGES += \
 
 #RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0
 
 # Sensors
 PRODUCT_PACKAGES += \
