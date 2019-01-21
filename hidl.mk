58bcae5fa7fbb42f273b6b9a8e927432c94632e5
Ben Grisdale <bengrisdale123@gmail.com>
2019-01-21 18:22:43 +0000

    athene: Build android.hidl.manager@1.0-java
     * Fixes: 03-02 11:14:06.806 810 810 E PackageManager: updateAllSharedLibrariesLPw failed: Package com.qualcomm.qcrilmsgtunnel requires unavailable shared library android.hidl.manager@1.0-java; failing!
    
    Change-Id: I53b3cb250690e64b7279e190e88b7567ea4e116d

--- hidl.mk
+++ hidl.mk
@@ -1,7 +1,8 @@
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # Audio
 PRODUCT_PACKAGES += \
