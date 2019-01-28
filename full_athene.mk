c5c5d1ea3a3cb9f59eb92b58d0736e29102fb576
kubersharma001 <kubersharma001@gmail.com>
2019-01-28 23:55:19 +0530

    athene: enable PRODUCT_ENFORCE_RRO_TARGETS for framework-res
    Change-Id: I4807f3ffe839947a4618c33ef58a1c6204519cbc

--- full_athene.mk
+++ full_athene.mk
@@ -25,3 +25,6 @@ $(call inherit-product, device/motorola/athene/device.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
 PRODUCT_NAME := full_athene
+
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
