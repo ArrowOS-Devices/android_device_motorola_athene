d3c0b4dd08044b8642780cf4553b4140f75222e2
Lyudmila17 <merkel.naller@gmail.com>
2019-02-16 13:46:18 +0100

    athene: Update GPS config from OPJ28.111-12
    Change-Id: Iba1d0157f08288e591874b36ea27e299164b3f76

--- device.mk
+++ device.mk
@@ -157,8 +157,10 @@ PRODUCT_PACKAGES += \
     libgnss \
     libgnsspps
 
+PRODUCT_PACKAGES += \
+    gps.conf
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
