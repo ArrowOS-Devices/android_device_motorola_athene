7c920ee1f7960d2df15ea7d9064e38ca78550261
Lyudmila17 <merkel.naller@gmail.com>
2019-07-04 01:21:34 +0200

    athene: Fixup vendor prop
     * Fixup prop
     * Remove duplicated prop

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -79,7 +79,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.sys.cnd.iwlan=1
 
 # Coresight
-persist.debug.coresight.config=stm-events
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.debug.coresight.config=stm-events
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -152,8 +153,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	media.msm8956hw=0 \
 	mm.enable.qcom_parser=135715 \
 	vidc.enc.dcvs.extra-buff-count=2 \
-	vidc.enc.narrow.searchrange=1 \
-	mm.enable.sec.smoothstreaming=false
+	vidc.enc.narrow.searchrange=1
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
